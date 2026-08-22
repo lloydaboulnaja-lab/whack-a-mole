427a801bf9869b985d7d7de8e01377b7c0e9aeca
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-08-22 21:12:09 +0100

    Add text font and enhance drawing functionality
    Added text font and improved drawing logic for circles.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ pygame.display.set_caption("Whack A Mole!")
 grass = (34, 139, 34)
 
 
+
 text_font = pygame.font.SysFont("Arial", 40, bold=True)
 
 FPS = 60
@@ -36,6 +37,8 @@ def draw(text, font, color, x, y,):
     img = font.render(text,True, color)
     WIN.blit(img, (x, y))
 
+
+
 class Mole:
     def __init__(self,mole_pos,mole_rect,appearance):
         self.mole_pos = mole_pos
@@ -50,6 +53,8 @@ class Mole:
         return self.appearance
         pygame.draw.rect()
     
+
+
 
 
 running = True
@@ -79,14 +84,31 @@ while running:
             else:
                 print("MISS")
 
+    
+
+    
+    
     pygame.draw.circle(WIN, (131, 101, 57), (190,230), 49)
+
+    pygame.draw.circle(WIN, (57, 45, 43), (190,230), 32)
+    pygame.draw.circle(WIN, (57, 45, 43), (165, 210), 10)  
+    pygame.draw.circle(WIN, (57, 45, 43), (215, 210), 10)  
+    pygame.draw.circle(WIN, (235, 150, 165), (165, 210), 5)  
+    pygame.draw.circle(WIN, (235, 150, 165), (215, 210), 5)  
+    pygame.draw.circle(WIN, (57, 45, 43), (190, 230), 32)
+    pygame.draw.circle(WIN, (0, 0, 0), (178, 225), 3)  
+    pygame.draw.circle(WIN, (0, 0, 0), (202, 225), 3)  
+    pygame.draw.circle(WIN, (235, 150, 165), (190, 245), 6)
+    pygame.draw.line(WIN, (0, 0, 0), (187, 252), (193, 252), 2)
+
+
     pygame.draw.circle(WIN, (131, 101, 57), (190+250,230), 49)
     pygame.draw.circle(WIN, (131, 101, 57), (190+250+250,230), 49)
-
     pygame.draw.circle(WIN, (131, 101, 57), (190,230+250), 49)
     pygame.draw.circle(WIN, (131, 101, 57), (190+250,230+250), 49)
     pygame.draw.circle(WIN, (131, 101, 57), (190+250+250,230+250), 49)
-   
+    
+
     draw("SCORE",text_font, (12,98,205), 20, 20)
    
     pygame.display.flip()
