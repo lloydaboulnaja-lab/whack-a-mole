4b7a872930691d7290f9295b3736f87242008af7
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-08-24 22:05:19 +0100

    Modify Mole class constructor to include position
    Updated Mole class to accept mole position as an argument.

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ pygame.display.set_caption("Whack A Mole!")
 
 grass = (34, 139, 34)
 
-
-
 text_font = pygame.font.SysFont("Arial", 40, bold=True)
 
 FPS = 60
@@ -40,11 +38,11 @@ def draw(text, font, color, x, y,):
 
 
 class Mole:
-    def __init__(self):
-        pass
+    def __init__(self,mole_pos):
 
+        self.mole_pos = mole_pos
 
-
+        
 
 running = True
 
