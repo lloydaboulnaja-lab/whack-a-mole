4036bc8f9df2b8f321310f1ec2bc7679106c3ff6
Lloyd <lloydaboulnaja@gmail.com>
2026-09-05 18:55:40 +0100

    changed playing part to function

--- main.py
+++ main.py
@@ -11,89 +11,86 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Whack A Mole!") 
 
-grass = (34, 139, 34)
-
-text_font = pygame.font.SysFont("Arial", 40, bold=True)
-
-FPS = 60
-
-score = 0 
 
 game_state = "playing"
 
-frames = pygame.time.Clock()
+def playing():
 
-positions = [
+    score = 0 
+
+    FPS = 60
+
+    frames = pygame.time.Clock()
+    
+    frames.tick(FPS)
+
+    grass = (34, 139, 34)
+
+    text_font = pygame.font.SysFont("Arial", 40, bold=True)
+
+
+    positions = [
 (190, 230),
 (440, 230),
 (690, 230),(190, 480),
 (440, 480),(690, 480)]
 
-rand_pos  = random.choice(positions)
+    rand_pos  = random.choice(positions)
 
 
-def draw(text, font, color, x, y,):  # drawing the score text at the top left of the window
-    img = font.render(text,True, color)
-    WIN.blit(img, (x, y))
+    def draw(text, font, color, x, y,):  # drawing the score text at the top left of the window
+        img = font.render(text,True, color)
+        WIN.blit(img, (x, y))
 
 
-class Mole:
+    class Mole:
 
-    ''' Created a mole object to own its attributes like its position
+        ''' Created a mole object to own its attributes like its position
     and to perform functions like moving itself
     '''
 
-    def __init__(self,pos):
+        def __init__(self,pos):
         
-        self.pos = pos
-        x,y = self.pos
+            self.pos = pos
+            x,y = self.pos
 
-        self.mole_rect = pygame.Rect(x-25,y-35,64,64)
+            self.mole_rect = pygame.Rect(x-25,y-35,64,64)
            
 
-    def draw_mole(self,surface):
+        def draw_mole(self,surface):
 
-        x,y = self.pos
+            x,y = self.pos
         
-        body = pygame.draw.circle(WIN, (57, 45, 43), self.pos, 32)
-        left_ear = pygame.draw.circle(WIN, (57, 45, 43), (x-25,y-20), 10)  
-        right_ear = pygame.draw.circle(WIN, (57, 45, 43), (x+25, y-20), 10)  
-        left_pink = pygame.draw.circle(WIN, (235, 150, 165), (x-25,y-20), 5)  
-        right_pink = pygame.draw.circle(WIN, (235, 150, 165), (x+25, y-20), 5)  
-        left_eye = pygame.draw.circle(WIN, (0, 0, 0), (x-12, y-5), 3)  
-        right_eye = pygame.draw.circle(WIN, (0, 0, 0), (x+12, y-5), 3)  
-        nose = pygame.draw.circle(WIN, (235, 150, 165), (x, y+15), 6)
+            body = pygame.draw.circle(WIN, (57, 45, 43), self.pos, 32)
+            left_ear = pygame.draw.circle(WIN, (57, 45, 43), (x-25,y-20), 10)  
+            right_ear = pygame.draw.circle(WIN, (57, 45, 43), (x+25, y-20), 10)  
+            left_pink = pygame.draw.circle(WIN, (235, 150, 165), (x-25,y-20), 5)  
+            right_pink = pygame.draw.circle(WIN, (235, 150, 165), (x+25, y-20), 5)  
+            left_eye = pygame.draw.circle(WIN, (0, 0, 0), (x-12, y-5), 3)  
+            right_eye = pygame.draw.circle(WIN, (0, 0, 0), (x+12, y-5), 3)  
+            nose = pygame.draw.circle(WIN, (235, 150, 165), (x, y+15), 6)
 
 
-    def change_pos(self):
+        def change_pos(self):
          
-        rand_pos = random.choice(positions)
+            rand_pos = random.choice(positions)
 
-        self.pos = rand_pos
+            self.pos = rand_pos
 
-        x, y = rand_pos
+            x, y = rand_pos
 
-        self.mole_rect.x = x - 25
-        self.mole_rect.y = y - 35
+            self.mole_rect.x = x - 25
+            self.mole_rect.y = y - 35
 
-        
+   
+    my_mole = Mole(rand_pos)
 
-my_mole = Mole(rand_pos)
-
-running = True
-
-last_moved = pygame.time.get_ticks()
-
-def playing_game():
-
-    frames.tick(FPS)
+    last_moved = pygame.time.get_ticks()
     
     current_time = pygame.time.get_ticks()
     
     if current_time - last_moved >= 1000:
         my_mole.change_pos()
-
-
         last_moved = current_time
             
     
@@ -113,31 +110,33 @@ def playing_game():
     
                 score += 1
                 print("HIT")
-    
             else:
-    
                 print("MISS")
     
     
-        pygame.draw.circle(WIN, (131, 101, 57), (190,230), 49)
-        pygame.draw.circle(WIN, (131, 101, 57), (440,230), 49)
-        pygame.draw.circle(WIN, (131, 101, 57), (690,230), 49)
-        pygame.draw.circle(WIN, (131, 101, 57), (190,480), 49)
-        pygame.draw.circle(WIN, (131, 101, 57), (440,480), 49)
-        pygame.draw.circle(WIN, (131, 101, 57), (690,480), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (190,230), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (440,230), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (690,230), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (190,480), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (440,480), 49)
+    pygame.draw.circle(WIN, (131, 101, 57), (690,480), 49)
     
         
-        my_mole.draw_mole(WIN)
+    my_mole.draw_mole(WIN)
     
-        draw(f"SCORE: {score}",text_font, (12,98,205), 20, 20)
+    
+    
+    #return "playing"
+    draw(f"SCORE: {score}",text_font, (12,98,205), 20, 20)
+    
 
-
+running = True
 
 while running:  # main game loop
 
     if game_state == "playing":
-        playing_game()
-    else:
-        pass
-   
+
+        game_state = playing()
+        
     pygame.display.flip()
+    
